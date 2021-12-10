22b5db8e1bc67f6f778ddd200d2a6cc5936de08f
Laura Rose Mary Hansen <laurarosemaryhansen@gmail.com>
2021-12-10 11:04:08 +0000

    Co-authored-by: compinschools <compinschools@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { ApiClient} from './ApiClient';
+import './App.css';
+import WeatherCard from "./weatherCard";
+
+
 
 function App() {
 
@@ -9,9 +13,6 @@ function App() {
   const api = new ApiClient();
 
  
-
-
-
 
 //getting data from api 
   const refreshQuote = () => {
@@ -31,57 +32,46 @@ function App() {
 
   useEffect(() => {
       refreshQuote();
-      
+    
   }, []);
 
-
-  function buildWeather() {
-      //is it an array with more than  one thing in
-      if(quotes.weather != undefined){
-        //give me back all items in the array
-        return quotes.weather.map ( (item) => {
-          //for each item in the array give me back some JSX
-          return (
-            <div>
-              {item.main}<br />
-              {item.description}<br />
-            </div>
-
-          )
-
-        }    )
-
-      }
-
-  }
-
-
-
-
-
-
+  
 //display funcition 
+
+/*
+  <WeatherCard day={quotes.day[0]} />
+    <WeatherCard day={quotes.day[1]} />
+    */
   return (
+
+
     <>
-      <h1>weather</h1>
-      {quotes.name}
-      <br></br>
-      {quotes.dt}
-      <br></br>
-      {quotes.coord != undefined? quotes.coord.lat : "" }
-      <br></br>
-      {quotes.coord != undefined? quotes.coord.lon : ""}
-      <br></br>
-      {quotes.main != undefined? quotes.main.temp : ""}
-      <br></br>
-      {quotes.main != undefined? quotes.main.feels_like: ""}
-      <br></br>
+    <div class="container">
+    <div class="main">
       
-     {buildWeather()}
-      
+    <WeatherCard day={quotes} />
+  
+
+       </div>
+       </div>
+       
+   
+     
       <br></br>
     </>
   );
 }
+       
+     
+
+
+
+       
+     
+
+     
+    
+
+
 
 export default App;
